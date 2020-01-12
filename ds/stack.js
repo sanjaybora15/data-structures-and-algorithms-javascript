93f1b52d653278d9c83aa17cb0598c52cea077d0
sanjaybora15 <sanjaybora15@gmail.com>
2020-01-12 23:00:36 +0530

    stack and queue updated

---
+++ ds/stack.js
@@ -0,0 +1,34 @@
+
+class Stack {
+    constructor () {
+      this.storage = {}
+      this.size = 0
+    }
+  
+    push(element) {
+      this.size++
+      this.storage[this.size] = element
+      console.log(this.storage)
+    }
+  
+    pop() {
+      let removed = this.storage[this.size]
+      delete this.storage[this.size]
+      this.size--
+      return removed
+    }
+  
+    peek() {
+      return this.storage[this.size]
+    }
+  }
+  
+  const stack = new Stack()
+  
+  stack.push('dog')
+  stack.push('cat')
+  stack.push('bear')
+  
+  stack.pop()
+  
+  stack.peek()

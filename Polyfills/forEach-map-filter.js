c23411b6f9febc3963524fc76dc3afe959b6602e
sanjay borah <sanjaybora15@gmail.com>
2022-05-14 14:52:18 +0530

    Update forEach-map-filter.js

--- Polyfills/forEach-map-filter.js
+++ Polyfills/forEach-map-filter.js
@@ -26,5 +26,36 @@ Array.prototype.newFilter = function(callback){
     return filtered;
 }
 
+
+
 let result = [1,2,3,4,5,6,7].newFilter(item => item > 2);
 console.log(result);
+
+
+//reduce
+Array.prototype.reduceAlbums = function(callback, initialValue) {
+  var accumulator = initialValue === undefined ? undefined : initialValue
+
+  for (var i = 0; i < this.length; i++) {
+    if (accumulator !== undefined) {
+      accumulator = callback.call(undefined, accumulator, this[i], i, this)
+    } else {
+      accumulator = this[i]
+    }
+  }
+  return accumulator
+} // our polyfill for reduce
+
+var logicAlbums = [
+  'Bobby Tarantino',
+  'The Incredible True Story',
+  'Supermarket',
+  'Under Pressure',
+]
+
+var combineAlbums = logicAlbums.reduceAlbums(function(a, b) {
+  return a + ' , ' + b
+}, 'Young Sinatra') // Initial Value is Young Sinatra
+
+console.log(combineAlbums)
+// Young Sinatra , Bobby Tarantino , The Incredible True Story , Supermarket , Under Pressure

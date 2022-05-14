40c7ff5fb544de416493be3c1d76cf830346896f
sanjay borah <sanjaybora15@gmail.com>
2022-05-14 14:19:35 +0530

    Update bind.js

--- Polyfills/bind.js
+++ Polyfills/bind.js
@@ -1,14 +1,19 @@
 let obj = {
-    address: 'Jack',
+    address: "bangalore",
 };
 
-let myFunction = (name, city) => {
-    return `Hi i am ${name} and i am from${this.address}, ${city}`
+function myFunction(name, city, pin) {
+    console.log(this.address)
+    return `Hi i am ${name} and i am from ${this.address}, ${city}, ${pin}`
 }
 
 Function.prototype.myBind = function (obj, ...args) {
-    return () => this.apply(obj, [...args])
+    var func = this
+    let o = obj
+    return function () {
+        return func.apply(o, [...args])
+    }
 }
 
-let newFunction = myFunction.myBind(obj)
-newFunction("")
+let newFunction = myFunction.myBind(obj, "sanjay", "karnataka", "560016")
+console.log(newFunction())

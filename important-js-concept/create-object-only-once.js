1bcf7861550c4e15dc6a51b8d14c4778b745674b
sanjay borah <sanjaybora15@gmail.com>
2022-05-14 13:03:25 +0530

    Create create-object-only-once.js

---
+++ important-js-concept/create-object-only-once.js
@@ -0,0 +1,27 @@
+// Write a singleton function to create an object only once
+// Singleton is a design pattern which restricts the creation of only one object from a given interface
+// When requested multiple times, same object is returned
+
+
+
+var Singleton = (function () {
+    var instance;
+ 
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+ 
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+// driver code
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();

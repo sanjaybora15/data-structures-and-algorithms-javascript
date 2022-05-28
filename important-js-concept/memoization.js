26da92d69f23c1f6cc22b0b54de57f6db47937e5
sanjay borah <sanjaybora15@gmail.com>
2022-05-28 12:07:09 +0530

    Update memoization.js

--- important-js-concept/memoization.js
+++ important-js-concept/memoization.js
@@ -28,3 +28,32 @@ const factorial = memoize(
 
 console.log(factorial(5));	// calculated
 console.log(factorial(6));	// calculated for 6 and cached for 5
+
+
+//Fibonacci
+//normal using loop
+/* var fibo = function(n) {
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return f;
+};
+
+
+console.log(fibo(9)) */
+
+//using memoization
+function fib(n, res = [0, 1, 1]) {
+    if (res[n]) {
+    console.log("call from cache")
+        return res[n];
+    }
+		console.log('call from out')
+    res[n] = fib(n - 1, res) + fib(n - 2, res);
+    return res[n];
+}
+
+console.log(fib(10));
